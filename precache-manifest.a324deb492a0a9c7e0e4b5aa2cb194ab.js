7f268b2a97d081a8c1beed05084051c130a75939
Jorge Antonio Romero <lestatloui@gmail.com>
2019-04-10 19:36:26 -0500

    Updates

---
+++ precache-manifest.a324deb492a0a9c7e0e4b5aa2cb194ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "6a94ea3cf0112e6bf766",
+    "url": "/contactmanager/static/js/main.6fbe3ce9.chunk.js"
+  },
+  {
+    "revision": "21b54d5d09529c89baf5",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "6a94ea3cf0112e6bf766",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "21b54d5d09529c89baf5",
+    "url": "/contactmanager/static/css/2.cab2a93a.chunk.css"
+  },
+  {
+    "revision": "9e3ab405f9dbf1985e418561058ee031",
+    "url": "/contactmanager/index.html"
+  }
+];
